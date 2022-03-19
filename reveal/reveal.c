f099697cf93d1a6ac6700c29c0c98339d8ca6956
fug <fug@mouse.info>
2022-03-19 06:40:50 -0400

    reveal script

--- reveal/reveal.c
+++ reveal/reveal.c
@@ -12,9 +12,9 @@ int main(int argc, char **argv){
 	char geth_args[1024] = {0};
 	snprintf(geth_args,sizeof(geth_args)-1,"geth attach http://127.0.0.1:8545 --exec \"loadScript(%s)\"",argv[1]);
 	printf("command: %s\n",geth_args);
-try_again:
+try_again:;
 	FILE *geth = popen(geth_args,"r");
-
+/*
 	struct pollfd poll_fd = {
 		 .fd = fileno(geth),
 		 .events = POLLIN,
@@ -32,7 +32,14 @@ try_again:
 		pclose(geth);
 		printf("error. try again\n");
 		goto try_again;
-	}
+	}*/
+	
+	char result[1024] = {0};
+		
+	fread(result,sizeof(result)-1,1,geth);
+		
+	printf("result: %s\n",result);
+	
 	printf("wait\n");
 	sleep(5);
 	printf("done waiting\n");
